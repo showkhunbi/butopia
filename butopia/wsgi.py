f8834b942400e599f0a9bb37516853c0054c0154
Shokunbi Johnson <maximusjshokes@gmail.com>
2020-11-20 19:32:02 +0100

    Initial Commit

---
+++ butopia/wsgi.py
@@ -0,0 +1,27 @@
+"""
+WSGI config for butopia project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'butopia.settings')
+
+# application = get_wsgi_application()
+
+
+from django.core.wsgi import get_wsgi_application
+import os
+import sys
+
+sys.path.append('/opt/bitnami/projects/butopia')
+os.environ.setdefault("PYTHON_EGG_CACHE",
+                      "/opt/bitnami/projects/butopia/egg_cache")
+os.environ["DJANGO_SETTINGS_MODULE"] = "butopia.settings"
+application = get_wsgi_application()
