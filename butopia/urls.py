fb46967cb14ee064f6eac8863245148bf93b86d0
Shokunbi Johnson <maximusjshokes@gmail.com>
2022-07-18 19:31:31 +0100

    Initial Commit

---
+++ butopia/urls.py
@@ -0,0 +1,32 @@
+"""butopia URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('adminshowkhunbi/', admin.site.urls),
+    path("", include("core.urls", namespace="core")),
+    path("app/", include("app.urls", namespace="app")),
+    path("phone/", include("phone.urls", namespace="phone")),
+    path('accounts/', include('allauth.urls')),
+]
+
+
+handler404 = "core.views.handler404"
+handler500 = "core.views.handler500"
+handler400 = "core.views.handler400"
+handler403 = "core.views.handler403"
